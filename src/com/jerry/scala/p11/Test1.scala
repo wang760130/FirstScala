016dba1d57462e88e327dc6be3b7c73ec398e7ff
JerryWang <jerry002@126.com>
2015-12-22 23:26:26 +0800

    trait

--- src/com/jerry/scala/p11/Test1.scala
+++ src/com/jerry/scala/p11/Test1.scala
@@ -8,6 +8,13 @@ package com.jerry.scala.p11
 
 import java.io.PrintWriter
 
+/**
+ * 在创建匿名类对象时，先调用的是Logger类的构造器，然后调用的是FileLogger的构造器。实际上构造器是按以下顺序执行的： 
+ * 1. 如果有超类，则先调用超类的构造器 
+ * 2. 如果有父trait，它会按照继承层次先调用父trait的构造器 
+ * 3. 如果有多个父trait，则按顺序从左到右执行 
+ * 4. 所有父类构造器和父trait被构造完之后，才会构造本类
+ */
 trait Logger {
   println("Logger")
   def log(msg:String) : Unit
