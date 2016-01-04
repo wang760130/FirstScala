2b816ad930d7bf8b6042b9535aac5a191f5863a9
JerryWang <jerry002@126.com>
2016-01-05 00:03:56 +0800

    Type Variable Bound

--- src/com/jerry/scala/p16/TypeVariableBound.scala
+++ src/com/jerry/scala/p16/TypeVariableBound.scala
@@ -25,10 +25,25 @@ case class Person(var name:String, var age : Int) extends Comparable[Person] {
   }
 }
 
+// 对类中的泛型进行范围限定
+// 定义Student类为case class，且泛型T的类型变量界定为AnyVal
+// 在创建类时，所有处于AnyVal类继承层次结构的类都是合法的
+// 如Int、Double等值类型
+case class Student[S,T <: AnyVal](var name:S,var hight:T)
+
 object TypeVariableBound {
   def main(args: Array[String]): Unit = {
     var typeVariableBound =  new TypeVariableBound
     println(typeVariableBound.compare("A", "B"))
     println(typeVariableBound.compare(Person("stephen",19), Person("john",20)))
+    
+    // 下面这条语句是不合法的，因为String类型不属于
+    // AnyVal类层次结构
+    // val s1 = Student("john","170")
+    
+    // 下面这两条语句都是合法的，因为
+    // Int,Long类型都是AnyVal
+    val S2=Student("john",170.0)
+    val S3=Student("john",170L)
   }
 }
