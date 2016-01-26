32c3d71b3aa3f7f110b496d4c5ea7ada0b1ad23f
JerryWang <jerry002@126.com>
2016-01-26 23:38:00 +0800

    Annotation

---
+++ src/com/jerry/scala/p16/AnnotationDemo.scala
@@ -0,0 +1,17 @@
+package com.jerry.scala.p16
+
+/**
+ * Scala入门到精通——第十六节 泛型与注解
+ * http://blog.csdn.net/lovehuangjiaju/article/details/47203847
+ * 注解（Annotation）简介
+ */
+/**
+ * Annotation是一种对程序代码进行描述的结构化信息。Annotation可以分布在程序的任何地方，能够注解变量、类、方法、参数等多种元素，它的主要功能有以下几种： 
+ */
+/**
+ * 1 自动生成scala文档
+ * 2 检查程序中可能出现的语法问题
+ */
+object AnnotationDemo {
+  
+}
