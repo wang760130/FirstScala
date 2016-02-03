e40941175ea1d4bd78d53c1d3d9150691257d4ec
JerryWang <jerry002@126.com>
2016-02-03 13:28:39 +0800

    TypeSpecialization

---
+++ src/com/jerry/scala/p24/TypeSpecializationDemo.scala
@@ -0,0 +1,10 @@
+package com.jerry.scala.p24
+
+/**
+ * Scala入门到精通——第二十四节 高级类型 （三）
+ * http://blog.csdn.net/lovehuangjiaju/article/details/47438567
+ * Type Specialization
+ */
+object TypeSpecializationDemo {
+  
+}
